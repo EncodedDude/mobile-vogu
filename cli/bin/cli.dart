643de4ce9eb0fc28b8ec53fd4967b8d6cc9c5d04
Dima Zotov <dima35zotov@mail.ru>
2026-05-19 19:03:38 +0300

    Создал функцию searchWikipedia и модифицировал функцию main, добавив ветвление аргумента search при запуске проекта, протестировал работоспособность тернарного оператора с выводом аргументов после search и с null

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -6,10 +6,15 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    print('Search command recognized!');
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  print('searchWikipedia received arguments: $arguments');
 }
 
 void printUsage() {
